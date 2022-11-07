f43c3c47139891c845d4b9f5d1ccc220158dfa43
al771 <58562943+al771@users.noreply.github.com>
2022-11-07 21:31:47 +0300

    Add files via upload

---
+++ 004A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main()
+{
+    int a = 0;
+    std::cin>>a;
+    if (a % 2 == 0 && a != 2){
+        std::cout<<"YES";
+    }
+    else{
+        std::cout<<"NO";
+    }
+}
